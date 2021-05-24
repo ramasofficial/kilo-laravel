6433614faaaa74b66c45f96ef4671a4b22e4a854
Ramas <web.sprendimai4@gmail.com>
2021-05-24 09:27:39 +0300

    Created models and migrations.

---
+++ database/migrations/2021_05_24_055244_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('category_id')->index();
+            $table->string('name');
+            $table->float('value', 5, 2);
+            $table->integer('quality');
+
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
